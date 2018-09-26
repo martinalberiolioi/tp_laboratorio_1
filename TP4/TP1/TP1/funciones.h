0e467be48c417efe1974a3a3e539301c7bddcc29
martinalberiolioi <martinalberiolioi@gmail.com>
2018-09-26 10:50:45 -0300

    agrego tp4

---
+++ TP4/TP1/TP1/funciones.h
@@ -0,0 +1,67 @@
+int primerNumero (void);
+
+/** \brief esta funcion captura el primer operando
+ * \param X es la variable donde se guarda el numero
+ * \return devuelve X, es decir el numero que capturó previamente
+ */
+
+int segundoNumero (void);
+
+/** \brief esta funcion captura el segundo operando
+ * \param Y es la variable donde se guarda el numero
+ * \return devuelve Y, es decir el numero que capturó previamente
+ */
+
+
+void sumar (int, int);
+
+/** \brief esta funcion suma dos numeros
+ * \param X es el primer numero a sumar
+ * \param Y es el segundo numero a sumar
+ * \param res es la variable donde se guarda el resultado de la suma
+ */
+
+void restar (int, int);
+
+/** \brief esta funcion resta dos numeros
+ * \param X es el primer numero a restar
+ * \param Y es el segundo numero a restar
+ * \param res es la variable donde se guarda el resultado de la resta
+ */
+
+
+void dividir (int, int);
+
+/** \brief esta funcion divide dos numeros
+ * \param X es el dividendo
+ * \param Y es el divisor
+ * \param res es la variable donde se guarda el resultado de la division
+ */
+
+void multiplicar (int, int);
+
+/** \brief esta funcion multiplica dos numeros
+ * \param X es el multiplicando
+ * \param Y es el multiplicador
+ * \param res es la variable donde se guarda el resultado de la multiplicacion
+ */
+
+
+void factorial (int);
+
+/** \brief esta funcion calcula el factorial de un numero
+ * \param X es el numero al que se le va a calcular el factorial
+ * \param res es la variable sobre la cual se calcula el factorial, y la que luego es mostrada como resultado
+ */
+
+void todas (int, int);
+
+/** \brief esta funcion suma, resta, divide y multiplica dos numeros. Calcula el factorial de un solo numero
+ * \param X es el primer operando
+ * \param Y es el segundo operando
+ * \param ressum es la variable donde se guarda el resultado de la suma
+ * \param resres es la variable donde se guarda el resultado de la resta
+ * \param resdiv es la variable donde se guarda el resultado de la division
+ * \param resmul es la variable donde se guarda el resultado de la multiplicacion
+ * \param res1 es la variable sobre la cual se calcula el factorial, y la que luego es mostrada como resultado
+ */
